c22704642301bdc4d41c88c2a28f056c0194666b
Hans Huebner <hans.huebner@gmail.com>
2015-02-01 14:48:21 -0500

    test support

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"]])
+                 [clj-time "0.9.0"]
+                 [org.clojure/tools.logging "0.3.1"]]
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.2.0"]
+                                  [ring/ring-core "1.3.2"]]}})
--- src/ring/middleware/common_log.clj
+++ src/ring/middleware/common_log.clj
@@ -7,11 +7,9 @@
 
 (defonce flush-interval 100)
 
-(def writer (agent {:filename "access.log"
-                    :max-file-size (* 10 1000 1000)
-                    :rotate? true}))
+(defonce writer (agent {}))
 
-(defonce file-timestamp-format (time-format/formatter "yyyyMMddHHmm"))
+(defonce file-timestamp-format (time-format/formatter "yyyyMMddHHmmss"))
 
 (defn- open-stream [agent]
   (assoc agent
@@ -25,50 +23,63 @@
       (log/error "could not rename log file" filename "to" archive-filename))))
 
 (defn- ensure-stream [agent]
-  (let [stream (:stream agent)]
-    (if (and stream
-             (or (not (:rotate? agent))
-                 (< (.length (io/file (:filename agent))) (:max-file-size agent))))
-      agent
+  (let [{:keys [stream max-file-size filename]} agent]
+    (cond
+      (and max-file-size
+           (<= max-file-size (.length (io/file filename))))
       (do (when stream
             (.close stream)
             (archive-current-file (:filename agent)))
-          (open-stream agent)))))
+          (open-stream agent))
 
-(defn- flush-lines [agent]
-  (if (:lines agent)
-    (let [agent (ensure-stream agent)]
-      (binding [*out* (:stream agent)]
-        (doseq [line (:lines agent)]
-          (println line))
-        (flush))
-      (dissoc agent :lines))
+      (not stream)
+      (open-stream agent)
+
+      true agent)))
+
+(defn- flush-lines [agent p]
+  (let [agent (if (:lines agent)
+                (let [agent (ensure-stream agent)]
+                  (binding [*out* (:stream agent)]
+                    (doseq [line (:lines agent)]
+                      (println line))
+                    (flush))
+                  (dissoc agent :lines))
+                agent)]
+    (when p
+      (deliver p true))
     agent))
 
 (defn- log [agent line]
-  (if (:lines agent)
-    (update-in agent [:lines] conj line)
-    (do (future (Thread/sleep flush-interval)
-                (send writer flush-lines))
-        (assoc agent
-               :lines [line]))))
+  (if (empty? (:filename agent))
+    (do (println line)
+        agent)
+    (if (:lines agent)
+      (update-in agent [:lines] conj line)
+      (do (future (Thread/sleep flush-interval)
+                  (send writer flush-lines nil))
+          (assoc agent
+                 :lines [line])))))
 
 (defn- reopen [agent options]
   (when (:stream agent)
-    (flush-lines agent)
+    (flush-lines agent nil)
     (.close (:stream agent)))
   options)
 
 (def line-timestamp-format (time-format/formatter "dd/MMM/YYYY:HH:mm:ss Z"))
 
-(def defaults {})
+(defn force-flush []
+  (let [p (promise)]
+    (send writer flush-lines p)
+    (deref p)))
 
-(defn wrap-with-common-log [handler & {:keys [filename max-file-size] :as options}]
+(defn wrap-with-common-log [handler {:keys [filename max-file-size] :as options}]
   (when (and filename
              max-file-size
              (= (.lastIndexOf filename ".") -1))
-    (throw (Exception. "log file name must contain a dot character if max-file-size is set so that the log timestamp can be filled in when reopening log file")))
-  (send writer reopen (merge defaults options) options)
+    (throw (Exception. ":filename must contain a dot character if :max-file-size is set so that the log timestamp can be filled in when reopening log file")))
+  (send writer reopen options)
   (fn [request]
     (let [response (handler request)
           {:keys [remote-addr request-method uri query-string] :or {remote-addr "-"}} request
