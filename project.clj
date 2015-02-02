655b2d5ea854e25057ffe3047572d4456a68a3a9
Hans Huebner <hans.huebner@gmail.com>
2015-02-01 20:41:02 -0500

    update project.clj with :scm info

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject ring.middleware.common-log "0.1.0"
   :description "A ring middleware to log requests in Common Log format"
   :url "https://github.com/hanshuebner/ring.middleware.common-log"
+  :scm {:name "git"
+        :url "https://github.com/hanshuebner/ring.middleware.common-log"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
